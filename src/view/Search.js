2724a99a276c502f6189263c8ce60898eeed3828
Mark Liang <mliang@mozilla.com>
2019-05-17 23:07:51 +0800

    update pollution

--- src/view/Search.js
+++ src/view/Search.js
@@ -11,6 +11,7 @@ import ReactList from 'react-list'; // eslint-disable-line no-unused-vars
 import Image from "react-graceful-image";
 import Swiper from 'swiper/dist/js/swiper.js';
 import Nav from '../component/Nav';
+import Cookies from 'universal-cookie';
 
 // Story Data
 const story_data = data.stories;
@@ -93,18 +94,54 @@ class Search extends Component {
     $(window).on('resize orientationchange', setHeight);
     
     // Init Page
+    const cookies = new Cookies();
+    var $t = this;
+
     document.body.classList.add('ds');
     document.getElementById('loading').classList.remove('fade');
 
-    // Preload Images
     var images  = [];
+    var loaded = false;
+    var p = 0;
+    var id = setInterval(frame, 10);
+    
+    function frame() {
+      //console.log(loaded)
+      if (p >= 100) {
+        if(loaded) {
+          if(cookies.get('firstVisit') === undefined) {
+            cookies.set('firstVisit', true, { path: '/' });
+            // window.location.reload();
+          }
+          setTimeout(function(){
+            document.getElementById('loading').classList.add('fade');
+            document.body.classList.remove('ds');
+          },400);
+          clearInterval(id);
+        }
+      } else {
+        p++; 
+        $('.progress-view').text(p+'%');
+      }
+    }
+    // var images  = [];
     loadImage(images)
     .then(function (allImgs) {
       console.log(allImgs.length, 'images loaded!', allImgs);
-      setTimeout(function(){
-        document.getElementById('loading').classList.add('fade');
-        document.body.classList.remove('ds');
-      },600);
+      console.log($t.state.view);
+      loaded = true;
+
+      if(p >= 100) {
+        clearInterval(id);
+        if(cookies.get('firstVisit') === undefined) {
+          cookies.set('firstVisit', true, { path: '/' });
+          // window.location.reload();
+        }
+        setTimeout(function(){
+          document.getElementById('loading').classList.add('fade');
+          document.body.classList.remove('ds');
+        },400);
+      }
     })
     .catch(function (err) {
       console.error('One or more images have failed to load :(');
@@ -179,7 +216,7 @@ class Search extends Component {
     if(s.time === "") {
       style = " bg-white dark-gray w7-ns w5 pa3";
       image = " dn";
-      if(this.state.topic <= 5) icon = (<img src={topic_data[this.state.topic].icon} width="44" height="44" className="pr3" alt={topic_data[this.state.topic].title}/>);
+      if(this.state.topic <= 5) icon = (<img src={topic_data[this.state.topic].icon} width="44" height="44" className="pr3 icon" alt={topic_data[this.state.topic].title}/>);
       else d = " dn";
     } else {
       image = " flex aic"
@@ -199,7 +236,7 @@ class Search extends Component {
     return (
       <li className={"storyItem item cp mh3"+d+style} key={i} onClick={(e) => this.onOpenModal(e, s)}>
         <div className="pn">
-          <figure className={"ma0 vh-25 overflow-hidden"+image} style={figure}>
+          <figure className={"ma0 vh-22 overflow-hidden"+image} style={figure}>
             <Image
               src={cover}
               width="100%"
@@ -327,16 +364,50 @@ class Search extends Component {
       images = this.state.content.images;
       imageLength = images.length;
     }
+
     var imageSlider = [];
-    for(var i = 0; i < imageLength; i++) {
-      var slides = (
-        <div className="swiper-slide" key={i}>
+    var all_content = null;
+    var label = null;
+    var bottomRight = {
+      bottom: this.state.mobile ? "45px": "0",
+      right: "0",
+      background: "rgba(0,0,0,.2)",
+      padding: this.state.mobile ? "10px" : "20px"
+    }
+
+    if(imageLength > 1) {
+      for(var i = 0; i < imageLength; i++) {
+        label = this.state.content.labels == undefined ? null : this.state.content.labels[i];
+        var slides = (
+          <div className="swiper-slide" key={i}>
+            <figure className="mh0 mv4 modalImg">
+              <img src={images[i]} alt="story" />
+              <label className="white absolute lh-normal f6-ns f8 pn" style={bottomRight}>{label}</label>
+            </figure>
+          </div>
+        )
+        imageSlider.push(slides);
+      }
+      all_content = (
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            {imageSlider}
+          </div>
+          <div className="swiper-pagination"></div>
+          <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div>
+        </div>
+      )
+    } else {
+      label = this.state.content.labels == undefined ? null : this.state.content.labels[0];
+      all_content = (
+        <div className="image">
           <figure className="mh0 mv4 modalImg">
-            <img src={images[i]} alt="story" />
+            <img src={images[0]} alt="story" />
+            <label className="white absolute lh-normal f6-ns f8 pn" style={bottomRight}>{label}</label>
           </figure>
         </div>
       )
-      imageSlider.push(slides);
     }
 
     var form = {
@@ -357,27 +428,32 @@ class Search extends Component {
       backgroundPosition: 'center center'
     }
 
+    
+
     return (
       <section id="timeline" className="min-vh-100 bg-white pv5-l pv4">
         <Helmet>
             <title>台灣環境史三十年大事紀 - 我們的島二十週年</title>
         </Helmet>
-        <Nav timeline={false} notfix={true}/>
+        <Nav timeline={false} notfix={true} show={true}/>
         <div className="mw8-ns center ph3-ns mb4-ns pt4-l pt5">
           <div className="cf mb3-ns mb2">
-            <div className="fl w-100 w-40-l flex aic mb3 mb0-l ph0-ns ph3">
+            <div className="fl w-100 w-20-l flex aic mb3 mb0-l ph0-ns ph3">
               <h2 className="ma0 nowrap">大事紀標題</h2>
-              <p className="ma0 ph3 o-50 f6">大事紀介紹大事紀介紹大事紀介紹大事紀介紹大事紀介紹</p>
             </div>
-            <div className="fl w-100 w-60-l">
+            <div className="fl w-100 w-80-l">
               <form className="flex space-between aic" style={form} onSubmit={this.updateSearch.bind(this)}>
                 <input id="search_input" className="w-100 ph2" type="text" ref="keyword" placeholder="搜尋紀事"/>
                 <select name="areas" ref="areas" className="w150">
                   <option value="">全部地區</option>
-                  <option value="north">北部地區</option>
-                  <option value="central">中部地區</option>
-                  <option value="south">南部地區</option>
-                  <option value="others">其他</option>
+                  <option value="area1">北北基</option>
+                  <option value="area2">桃竹苗</option>
+                  <option value="area3">中彰投</option>
+                  <option value="area4">雲嘉南</option>
+                  <option value="area5">高屏</option>
+                  <option value="area6">宜花東</option>
+                  <option value="area7">金馬澎蘭嶼</option>
+                  <option value="others">不分區及其他</option>
                 </select>
                 <input type="submit" className="ph2 tc cp w4" value="搜尋" />
               </form>
@@ -399,20 +475,10 @@ class Search extends Component {
           </div>
         </div>
         <Modal open={open} onClose={this.onCloseModal}>
-          <div className="swiper-container">
-            <div className="swiper-wrapper">
-              {imageSlider}
-            </div>
-            <div className="swiper-pagination"></div>
-            <div className="swiper-button-prev"></div>
-            <div className="swiper-button-next"></div>
-          </div>
-          
+          {all_content}
           <div className="ph4-ns pb4 modalTxt">
-            <h2>{this.state.content.name}</h2>
-            <p className="lh-copy">
-              {this.state.content.content}
-            </p>
+            <h2 className="f3-ns f5">{this.state.content.name}</h2>
+            <p className="lh-copy pre-wrap" dangerouslySetInnerHTML={{__html:this.state.content.content}}></p>
           </div>
         </Modal>
       </section>
